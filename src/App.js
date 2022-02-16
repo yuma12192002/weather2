86eaae67e92399d364c4e92ea4a549b1efd7baa6
Yuma sakai <21aw0121@jec.ac.jp>
2022-02-16 21:50:52 +0900

    完成

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [weather, selectWether] = useState(['晴れ']);
+  
+  const handleChange2 = (event) => {
+    selectWether(event.target.value);
+  }
+
+  const h1Style = {
+    fontSize : '15px',
+  }
+
+  useEffect (() =>{
+    alert(`天気は${weather.toString()}`)
+  })
+
+  return(
+  <>
+    <div>
+      <h1 id='h1-ttl' style={h1Style}>天気</h1>
+      <p>場所を選んでください</p>
+        <select>
+            <option value="tokyo">東京</option>
+            <option value="osaka">大阪</option>
+            <option value="sapporo">札幌</option>
+        </select>
+      <p>時間を選んでください</p>
+      <select onChange={handleChange2}>
+          <option value="晴れ">AM 9:00</option>
+          <option value="雨">AM 12:00</option>
+          <option value="くもり">AM 15:00</option>
+      </select>
     </div>
-  );
+  </>
+  )
 }
 
-export default App;
+  export default App;
+
